e6b26682fc7740e95a5397e74b2db40f6d1479d5
Prince Mishra <prince.mishra1384@gmail.com>
2022-12-22 20:43:10 +0530

    first commit

---
+++ database.js
@@ -0,0 +1,111 @@
+const { initializeApp } = require("firebase/app");
+const {
+  getFirestore,
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  setDoc,
+  doc,
+  updateDoc,
+} = require("firebase/firestore");
+
+const {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} = require("firebase/auth");
+
+const users = require("./public/json/users.json");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTWK9PrB0NqrvujY4xQID3c72SdNpLDsw",
+  authDomain: "stickman-78f30.firebaseapp.com",
+  projectId: "stickman-78f30",
+  storageBucket: "stickman-78f30.appspot.com",
+  messagingSenderId: "988800194409",
+  appId: "1:988800194409:web:bdba98324b76559d6bcff8",
+  measurementId: "G-PPTQ4L7GQ7",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const insert = async (collec, data) => {
+  try {
+    const docRef = await addDoc(collection(db, collec), data);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+const getData = async (collec, q = "") => {
+  const col = collection(db, collec);
+
+  if (q != "") {
+    querySnapshot = await getDocs(q);
+  } else {
+    querySnapshot = await getDocs(col);
+  }
+
+  res = new Array();
+  querySnapshot.forEach((doc) => {
+    res.push(doc.data());
+  });
+
+  return res;
+};
+
+const createUser = (email, pass) => {
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, pass)
+    .then((userCredential) => {
+      // const user = userCredential.user;
+      console.log("signed");
+    })
+    .catch((error) => {
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+      // ..
+      console.log("not signed");
+    });
+};
+
+const signInUser = (email, pass) => {
+  const auth = getAuth();
+  return signInWithEmailAndPassword(auth, email, pass);
+};
+
+const getAllData = async (startdate = "", enddate = "") => {
+  let cnt = 10000;
+  let allEntries = [];
+
+  for (let index = 0; index < users.length; index++) {
+    const coll = users[index].email;
+
+    const entries = await getData(coll);
+    entries.forEach((entry) => {
+      entry["numbers"].forEach((number) => {
+        let helper = new Array();
+        cnt = cnt + 1;
+        helper.push(cnt);
+        helper.push(entry["name"]);
+        helper.push(number);
+        helper.push(entry["date_added"].toDate());
+
+        if (startdate=="" || (helper[3]-startdate >= 0 && helper[3]- enddate <= 0)) {
+          allEntries.push(helper);
+        }
+      });
+    });
+  }
+
+  allEntries.sort((a, b) => {
+    return a[3] - b[3];
+  });
+
+  return allEntries;
+};
+module.exports = { insert, getData, createUser, signInUser, getAllData };
---
+++ index.js
@@ -0,0 +1,188 @@
+const express = require("express");
+const app = express();
+const users = require("./public/json/users.json");
+const admin = require("./public/json/admin.json");
+const puppeteer = require("puppeteer");
+const { Timestamp } = require("firebase/firestore");
+
+const {
+  insert,
+  getData,
+  createUser,
+  signInUser,
+  getAllData,
+} = require("./database");
+const { render } = require("ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  context = {
+    users: users,
+    admin: admin,
+  };
+
+  res.render("index", context);
+});
+
+app.get("/login/:worker", (req, res) => {
+  const context = {
+    isError: false,
+    page: req.params["worker"],
+  };
+
+  res.render("login", context);
+});
+
+app.post("/admin", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email != "admin@stickman.com") {
+    context = {
+      isError: true,
+      error: "Please enter the admin email id",
+      page: "admin",
+    };
+    res.render("login", context);
+  } else {
+    signInUser(email, password)
+      .then((userdata) => {
+        getAllData()
+          .then((data) => {
+            res.render("admin", { email: email, entries: data });
+          })
+          .catch((err) => {
+            context = {
+              users: users,
+              admin: admin,
+            };
+
+            res.render("index", context);
+          });
+      })
+      .catch((err) => {
+        const context = {
+          isError: true,
+          error: "wrong password",
+          page: "admin",
+        };
+        res.render("login", context);
+      });
+  }
+});
+
+app.post("/user", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  let flag = false;
+
+  users.forEach((user) => {
+    if (user["email"] == email) flag = true;
+  });
+
+  if (flag == false) {
+    context = {
+      isError: true,
+      error: "Please enter the user email id",
+      page: "user",
+    };
+    res.render("login", context);
+  } else {
+    const per = (await getData("persons"))[0]["name"];
+
+    signInUser(email, password)
+      .then((userdata) => {
+        const context = {
+          email: email,
+          persons: per,
+          showData: false,
+        };
+
+        res.render("users", context);
+      })
+      .catch((err) => {
+        const context = {
+          isError: true,
+          error: "wrong password",
+          page: "user",
+        };
+        res.render("login", context);
+      });
+  }
+});
+
+app.post("/addData", async (req, res) => {
+  const email = req.body.email;
+  const name = req.body.name;
+  let numbers = [];
+  delete req.body.email;
+  delete req.body.name;
+
+  for (const key in req.body) numbers.push(req.body[key]);
+
+  data = {
+    name: name,
+    numbers: numbers,
+    date_added: Timestamp.fromDate(new Date()),
+  };
+
+  insert(email, data);
+  const per = (await getData("persons"))[0]["name"];
+
+  // getting all all feed
+  let allEntries = new Array();
+
+  const entries = await getData(email);
+  let cnt = 10001;
+
+  entries.forEach((mp) => {
+    console.log(mp);
+    mp["numbers"].forEach((num) => {
+      let helper = new Array();
+      helper.push(cnt);
+      helper.push(mp["name"]);
+      helper.push(num);
+      helper.push(mp["date_added"].toDate().toDateString());
+      cnt = cnt + 1;
+      allEntries.push(helper);
+    });
+  });
+
+  const context = {
+    email: email,
+    persons: per,
+    entries: allEntries,
+    showData: true,
+  };
+
+  res.render("users", context);
+});
+
+
+app.get("/getAllData", async (req, res) => {
+  let startDate = new Date(req.query.startDate);
+  let endDate = new Date(req.query.endDate);
+  endDate.setDate(endDate.getDate() + 1)
+
+  getAllData(startDate,endDate)
+    .then((data) => {
+      res.render("admin", { entries: data });
+    })
+    .catch((err) => {
+      context = {
+        users: users,
+        admin: admin,
+      };
+
+      res.render("index", context);
+    });
+});
+
+
+app.listen(4500);
