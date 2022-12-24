aa4f3b6cc8ebff2f43f91f238efe5cf109fea0e1
Prince Mishra <prince.mishra1384@gmail.com>
2022-12-24 16:03:16 +0530

    added static folder

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const { insert, getData, signInUser, getAllData } = require("./database");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "temp")));
+app.use(express.static(path.join(__dirname, "tmp")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
